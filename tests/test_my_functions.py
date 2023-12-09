ac3bcdddd83a7a8111b99ee8018c7921b4d090a4
Leonardo H. Batista <leonardo.batista@oneblinc.com>
2023-12-09 13:30:16 -0300

    Mark examples

--- tests/test_my_functions.py
+++ tests/test_my_functions.py
@@ -1,3 +1,5 @@
+import time
+
 import pytest
 
 from source.my_functions import add, divide
@@ -21,3 +23,20 @@ def test_divide():
 def test_divide_by_zero():
     with pytest.raises(ZeroDivisionError):
         divide(10, 0)
+
+
+@pytest.mark.slow
+def test_very_slow():
+    time.sleep(3)
+    result = divide(10, 2)
+    assert result == 5
+
+
+@pytest.mark.skip(reason="Not implemented yet")
+def test_not_implemented():
+    assert False
+
+
+@pytest.mark.xfail(reason="We know that this fails")
+def test_divide_by_zero_xfail():
+    divide(10, 0)
