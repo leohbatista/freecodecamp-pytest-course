1baca206f6f86a3761b4c6c29067849c770ff674
Leonardo H. Batista <leonardo.batista@oneblinc.com>
2023-12-09 15:14:02 -0300

    Parametrize examples

---
+++ tests/test_square.py
@@ -0,0 +1,13 @@
+import pytest
+
+import source.shapes as shapes
+
+
+@pytest.mark.parametrize("side_length, expected_area", [(1, 1), (2, 4), (3, 9)])
+def test_multiple_square_areas(side_length, expected_area):
+    assert shapes.Square(side_length).area() == expected_area
+
+
+@pytest.mark.parametrize("side_length, expected_perimeter", [(1, 4), (2, 8), (3, 12)])
+def test_multiple_square_perimeters(side_length, expected_perimeter):
+    assert shapes.Square(side_length).perimeter() == expected_perimeter
